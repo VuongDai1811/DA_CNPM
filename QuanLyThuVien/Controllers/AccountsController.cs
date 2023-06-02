058eff1e1a70a85366bdcddf7112ee182fc70d02
VuongDai1811 <vuongdai18112001@gmail.com>
2023-06-02 15:01:32 +0700

    new

---
+++ QuanLyThuVien/Controllers/AccountsController.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuanLyThuVien.Extension;
+using QuanLyThuVien.Helpper;
+using QuanLyThuVien.Models;
+using QuanLyThuVien.ModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly dbLibraryContext _context;
+        public AccountsController(dbLibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ValidatePhone(string Phone)
+        {
+            try
+            {
+                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.Phone.ToLower() == Phone.ToLower());
+                if (khachhang != null)
+                    return Json(data: "Số điện thoại : " + Phone + " Đã được sử dụng ");
+                return Json(data: true);
+            }
+            catch
+            {
+                return Json(data: true);
+            }
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ValidateEmail(string Email)
+        {
+            try
+            {
+                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.Email.ToLower() == Email.ToLower());
+                if (khachhang != null)
+                    return Json(data: "Email : " + Email + " Đã được sử dụng <br />");
+                return Json(data: true);
+            }
+            catch
+            {
+                return Json(data: true);
+            }
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("dang-ky.html", Name = "DangKy")]
+        public IActionResult DangKyTaiKhoan()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("dang-ky.html", Name = "DangKy")]
+        public async Task<IActionResult> DangKyTaiKhoan(RegisterVM taikhoan)
+        {
+            try
+            {
+                // Check tài khoản trước khi đăng ký
+//                if (IshasAccount(taikhoan))
+//                {
+//                    return View();
+//                }
+                if (ModelState.IsValid)
+                {
+                    string salt = Utilities.GetRandomKey();
+                    Customers khachhang = new Customers
+                    {
+                        FullName = taikhoan.FullName,
+                        Phone = taikhoan.Phone.Trim().ToLower(),
+                        Email = taikhoan.Email.Trim().ToLower(),
+                        Password = (taikhoan.Password + salt.Trim()).ToMD5(),
+                        Active = true,
+                        Salt = salt,
+                        CreateDate = DateTime.Now
+                    };
+                    try
+                    {
+                        _context.Add(khachhang);
+                        await _context.SaveChangesAsync();
+                        // Lưu Session MaKh
+                        HttpContext.Session.SetString("CustomerId", khachhang.CustomerId.ToString());
+                        var taikhoanID = HttpContext.Session.GetString("CustomerId");
+                        //Identity
+                        var claims = new List<Claim>
+                        {
+                            new Claim(ClaimTypes.Name, khachhang.FullName),
+                            new Claim("CustomerId", khachhang.CustomerId.ToString())
+                        };
+                        ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "login");
+                        ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                        await HttpContext.SignInAsync(claimsPrincipal);
+
+/*                        var ShoppingCart = GioHang;
+                        if (ShoppingCart.Count > 0) return RedirectToAction("Shipping", "Checkout");    */
+
+                        return RedirectToAction("Dashboard", "Accounts");
+                    }
+                    catch
+                    {
+                        return RedirectToAction("DangKyTaiKhoan", "Accounts");
+                    }
+                }
+                else
+                {
+                    return View(taikhoan);
+                }
+            }
+            catch
+            {
+                return View(taikhoan);
+            }
+        }
+        [AllowAnonymous]
+        [Route("dang-nhap.html", Name = "DangNhap")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            var taikhoanID = HttpContext.Session.GetString("CustomerId");
+            if (taikhoanID != null)
+            {
+                return RedirectToAction("Dashboard", "Account");
+            }
+//            ViewBag.ReturnUrl = returnUrl;
+            // Check nếu giỏ hàng đã có thì trả về giỏ hàng để khách hàng thấy
+
+//            ViewBag.ShoppingCarts = GioHang;
+
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("dang-nhap.html", Name = "DangNhap")]
+        public async Task<IActionResult> Login(LoginViewModel customer, string returnUrl = null)
+        {
+            try
+            {
+//                if (!CheckUserName(model.UserName.Trim())) return View(model);
+
+                if (ModelState.IsValid)
+                {
+                    bool isEmail = Utilities.IsValidEmail(customer.UserName);
+                    if (!isEmail) return View(customer);
+
+                    var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.Email.Trim() == customer.UserName);
+
+                    if (khachhang == null) return RedirectToAction("DangKyTaiKhoan");
+                    string pass = (customer.Password + khachhang.Salt.Trim()).ToMD5();
+                    if (khachhang.Password != pass)
+                    {
+                        ViewBag.Error = "+ Sai thông tin đăng nhập";
+                        return View(customer);
+                    }
+                    // Kiểm tra xem account có bị disable hay ko
+
+                    if (khachhang.Active == false) return RedirectToAction("ThongBao", "Account");
+
+                    // Luu Session MaKh
+                    HttpContext.Session.SetString("CustomerId", khachhang.CustomerId.ToString());
+                    var taikhoanID = HttpContext.Session.GetString("CustomerId");
+                    // Identity
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, khachhang.FullName),
+                        new Claim("CustomerId", khachhang.CustomerId.ToString())
+                    };
+                    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "login");
+                    ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                    await HttpContext.SignInAsync(claimsPrincipal);
+/*                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        ViewBag.ShoppingCarts = GioHang;
+                        return Redirect(returnUrl);
+                    }   */
+                    return RedirectToAction("Shipping", "Checkout");
+                }
+            }
+            catch
+            {
+                return RedirectToAction("DangKyTaiKhoan", "Accounts");
+            }
+
+            return View(customer);
+        }
+/*        public bool CheckUserName(string username)
+        {
+            bool isok = false;
+            if (username.IndexOf('@') > -1)
+            {
+                //Validate email format
+                string emailRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                                        @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                                            @".)+))([a-zA-Z]{2.4}|[0-9]{1.3})(\]?)$";
+                Regex re = new Regex(emailRegex);
+                if (!re.IsMatch(username))
+                {
+
+                }
+            }
+        }   */
+    }
+}
